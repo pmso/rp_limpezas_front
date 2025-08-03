d0d0230022aa67a9982e6848840d05da155e3f07
pmso <pedro-mig@hotmail.com>
2025-08-03 19:48:18 +0100

    Login flow working

--- lib/enum/storageKeysEnum.dart
+++ lib/enum/storageKeysEnum.dart
@@ -3,7 +3,9 @@ enum StorageKeysEnum {
   refreshToken('refresh'),
   firstTime('first_time'),
   userId('user_id'),
-  userEmail('user_email');
+  userEmail('user_email'),
+  userUsername('username'),
+  userName('user_name');
 
   final String key;
   const StorageKeysEnum(this.key);
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,12 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:rp_limpezas_front/enum/storageKeysEnum.dart';
+import 'package:rp_limpezas_front/service/apiService.dart';
+import 'package:rp_limpezas_front/service/storageService.dart';
 
 class LoginPage extends StatelessWidget {
+
   const LoginPage({super.key});
+
+  void login(BuildContext context, TextEditingController usernameController, TextEditingController passwordController) async{
+    String username = usernameController.text;
+    String password = passwordController.text;
+    print('Login button pressed test $username $password');
+
+    print('{"username_email":"$username","password":"$password"}');
+
+    Apiservice.postData('auth/login','{"username_email":"$username","password":"$password"}').then((value) {
+      String accessToken;
+      String refreshToken;
+      String user_id;
+      String user_email;
+      String username;
+      String user_name;
+
+    accessToken = value['accessToken'];
+    refreshToken = value['refreshToken'];
+
+    user_id = value['user']['id'];
+    user_email = value['user']['email'];
+    username = value['user']['username'];
+    user_name = value['user']['name'] ?? '';
+
+    StorageService.saveAccessToken(accessToken);
+    StorageService.saveRefreshToken(refreshToken);
+
+    StorageService.setGenericValue(StorageKeysEnum.userId.key, user_id);
+    StorageService.setGenericValue(StorageKeysEnum.userEmail.key, user_email);
+    StorageService.setGenericValue(StorageKeysEnum.userUsername.key, username);
+    StorageService.setGenericValue(StorageKeysEnum.userName.key, user_name);
+
+    Navigator.pushReplacementNamed(context, '/home');
+
+  }).catchError((error) async {
+    print('Erro ao obter token: $error');
+    await StorageService.deleteTokens();
+  });
+  }
 
   @override
   Widget build(BuildContext context) {
-    final TextEditingController usernameController = TextEditingController();
-    final TextEditingController passwordController = TextEditingController();
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
     return Scaffold(
       appBar: AppBar(title: const Text('Login')),
       body: Center(
@@ -36,7 +79,7 @@ class LoginPage extends StatelessWidget {
                 width: double.infinity,
                 child: ElevatedButton(
                   onPressed: () {
-                    // TODO: Implement login logic
+                    login(context,usernameController,passwordController); // Call the login function here
                   },
                   child: const Text('Login'),
                 ),
